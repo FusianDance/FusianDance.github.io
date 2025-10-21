2e17a87af34f4553341b722c596a744b5036c270
dennis <dp.jandow@gmail.com>
2025-10-21 12:20:19 +0200

    add more insta posts

--- fusian-web/src/app/gallery/page.tsx
+++ fusian-web/src/app/gallery/page.tsx
@@ -8,7 +8,7 @@ export default function GalleryPage() {
 
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
-      <div className="max-w-4xl mx-auto">
+      <div className="w-full mx-auto">
         <h1 className="text-4xl font-bold mb-8">Gallery</h1>
         <div className="flex flex-wrap gap-4">
           {posts?.map((post) => (
